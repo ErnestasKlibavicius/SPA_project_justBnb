db2d94b40a4b65d6909d6621167ec8fca512ddad
Ernestas Klibavicius <ernestas.klibavicius@gmail.com>
2022-12-14 23:13:41 +0200

    design updates

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,28 +1,52 @@
 <template>
-    <div>
-        <nav class="navbar bg-white border-bottom navbar-light">
+    <div class="flex-wrapper">
+<!--        <nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--            <a class="navbar-brand" href="#">Navbar</a>-->
+<!--            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"-->
+<!--                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                <span class="navbar-toggler-icon"></span>-->
+<!--            </button>-->
+<!--            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--                <div class="navbar-nav">-->
+<!--                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>-->
+<!--                    <a class="nav-item nav-link" href="#">Features</a>-->
+<!--                    <a class="nav-item nav-link" href="#">Pricing</a>-->
+<!--                    <a class="nav-item nav-link disabled" href="#">Disabled</a>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </nav>-->
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
             <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }">JustBnb</router-link>
 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div v-if="!userAuthData">
-                <router-link class="btn nav-button" :to="{ name: 'login'}">
-                    Login
-                </router-link>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <div v-if="!userAuthData">
+                        <router-link class="btn nav-button navai" :to="{ name: 'login'}">
+                            Login
+                        </router-link>
 
-                <router-link class="btn nav-button" :to="{ name: 'register'}">
-                    Register
-                </router-link>
+                        <router-link class="btn nav-button navai" :to="{ name: 'register'}">
+                            Register
+                        </router-link>
+                    </div>
 
+                    <div v-if="userAuthData">
+                        <span class="btn nav-button navai" @click="Logout">Logout</span>
+
+                        <router-link class="btn nav-button navai" :to="{ name: 'basket' }">
+                            Basket
+                            <span v-if="itemsInBasket" class="badge badge-secondary"> {{ itemsInBasket }}</span>
+                        </router-link>
+                    </div>
+                </div>
             </div>
 
-            <div v-if="userAuthData">
-                <span class="btn nav-button" @click="Logout">Logout</span>
-
-                <router-link class="btn nav-button" :to="{ name: 'basket' }">
-                    Basket
-                    <span v-if="itemsInBasket" class="badge badge-secondary"> {{ itemsInBasket }}</span>
-                </router-link>
-            </div>
 
         </nav>
 
@@ -30,6 +54,56 @@
             <router-view></router-view>
         </div>
 
+        <footer style="background-color: #2196f3 !important; color: #fff;"
+                class="index-footer page-footer font-small pt-4">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                    <div class="col-md-6 mt-md-0 mt-3">
+                        <h5 class="text-uppercase">Footer Content</h5>
+                        <p>Here you can use rows and columns to organize your footer content.</p>
+                    </div>
+                    <hr class="clearfix w-100 d-md-none pb-3">
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <h5 class="text-uppercase">Links</h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 mb-md-0 mb-3">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div style="background-color: #1e7cc6;" class="footer-copyright text-center py-3">© 2022 Copyright:
+                <a href="/"> JustBnb</a>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -59,7 +133,7 @@ export default {
                 // let userToken = JSON.parse(localStorage.getItem('userData')).authData.token;
                 let axiosInstance = axios.create({
                     headers: {
-                        Authorization : `Bearer ${this.userAuthData.authData.token}`
+                        Authorization: `Bearer ${this.userAuthData.authData.token}`
                     }
                 });
 
